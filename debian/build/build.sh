ca68bed2607bd87f31c5019429f089b74f0e3d72
Sebastien Badia <sebastien@clustree.com>
2018-03-19 17:17:06 +0100

    d/build: Also build source-package

--- debian/build/build.sh
+++ debian/build/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -xe
 mk-build-deps -i -r -t 'apt-get -y' /opt/bird-exporter-deb/debian/control
-pushd /opt/bird-exporter-deb && gbp buildpackage $GIT_BUILDPACKAGE_OPTIONS
+pushd /opt/bird-exporter-deb && gbp buildpackage $GIT_BUILDPACKAGE_OPTIONS && gbp buildpackage -S -d --git-builder="debuild -eGOPATH -eGIT_DESCRIBE -eGIT_COMMIT -i -I -uc -us"
 cp /tmp/build-area/* /tmp/bird-exporter-deb
